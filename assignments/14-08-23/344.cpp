261b41c6603dc892c9d24d2019751082c81ffbec
Jay Raval <jay.mrugesh.raval@gmail.com>
2023-08-14 14:06:31 +0530

    Date:14/08/23 Jay Raval

---
+++ assignments/14-08-23/344.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s, int pos = 0) {
+        if (pos < s.size() / 2) {
+            swap(s[pos], s[s.size() - 1 - pos]);
+            reverseString(s, pos + 1);
+        }
+    }
+};
